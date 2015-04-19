5f39a20adf7f2396d103e2f48cdcfd6047d142a5
theakaterina <theakoutsoukis@gmail.com>
2015-04-19 20:39:53 +1000

    Added task on using the math library

--- lesson3/task4/tests.py
+++ lesson3/task4/tests.py
@@ -1,21 +1,28 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
+from test_helper import run_common_tests, failed, passed, get_answer_placeholders, get_file_output
+from math import pi
 
 
 def test_answer_placeholders():
     placeholders = get_answer_placeholders()
-    placeholder = placeholders[0]
-    if placeholder == "\"Hello World!\"":
+    radius = placeholders[0]
+    formula = placeholders[1]
+    if radius == "5":
         passed()
-    elif placeholder == "\'Hello World!\'":
-        passed()
-    elif placeholder == "Hello World!":
-        failed("Have you put quotation marks around the words?")
     else:
-        failed("Make sure the capitalization and punctuation is exactly the same as the example")
+        failed("The radius of the circle should be 5")
+    if "pi" in formula:
+        passed()
+    else:
+        failed()
 
 
 if __name__ == '__main__':
     run_common_tests()
     test_answer_placeholders()
+    answer = pi * 25
+    if get_file_output() == [str(answer)]:
+        passed()
+    else:
+        failed("Something is wrong with your formula")
 
 
