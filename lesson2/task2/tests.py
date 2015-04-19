acf86725ceeaad9ee9e5eb97ea23423c725c05b5
theakaterina <theakoutsoukis@gmail.com>
2015-04-19 23:07:29 +1000

    Html finished

--- lesson2/task2/tests.py
+++ lesson2/task2/tests.py
@@ -4,14 +4,14 @@ from test_helper import run_common_tests, failed, passed, get_answer_placeholder
 def test_answer_placeholders():
     placeholders = get_answer_placeholders()
     placeholder = placeholders[0]
-    if placeholder == "":  # TODO: your condition here
+    if placeholder == "str(integer)":
         passed()
     else:
-        failed()
+        failed("Convert integer to a string using the str() method")
 
 
 if __name__ == '__main__':
     run_common_tests()
-    # test_answer_placeholders()       # TODO: uncomment test call
+    test_answer_placeholders()
 
 
