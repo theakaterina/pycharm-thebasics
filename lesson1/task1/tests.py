8883fb0544ee8b2424bcd36b16a438bb66bbd7a6
theakaterina <theakoutsoukis@gmail.com>
2015-05-08 20:51:25 +1000

    Made the hello world task a little less specific

--- lesson1/task1/tests.py
+++ lesson1/task1/tests.py
@@ -1,19 +1,4 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
-
-
-def test_answer_placeholders():
-    placeholders = get_answer_placeholders()
-    placeholder = placeholders[0]
-    if placeholder == "\"Hello World!\"":
-        passed()
-    elif placeholder == "'\Hello World!\'":
-        passed()
-    elif placeholder == "Hello World!":
-        failed("Have you put quotation marks around the words?")
-    else:
-        failed("Make sure the capitalization and punctuation is exactly the same as the example")
-
+from test_helper import run_common_tests
 
 if __name__ == '__main__':
     run_common_tests()
-    test_answer_placeholders()
